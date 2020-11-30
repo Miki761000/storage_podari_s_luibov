d3cf98e796f7f43355f563fff3b17f2d8054a99c
M T <miki761000@gmail.com>
2020-11-30 23:33:51 +0200

    paging

--- warehouse/validators.py
+++
@@ -1,7 +0,0 @@
-from django.core.exceptions import ValidationError
-
-
-def positive_number(value):
-    result = value >= 0
-    if not result:
-        raise ValidationError('Quantity must be positive number')
